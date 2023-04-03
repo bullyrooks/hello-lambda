1ae5809f358bd96c659fef73295b16f36223ebc9
brian <brian.rook@gmail.com>
2023-04-03 17:45:13 -0600

    feat: test custom domain name

--- hello_lambda/hello_lambda_stack.py
+++ hello_lambda/hello_lambda_stack.py
@@ -4,11 +4,13 @@ from aws_cdk import (
     # Duration,
     Stack,
     aws_lambda as _lambda,
-    aws_apigateway as apigateway)
+    aws_apigateway as apigateway,
+    aws_certificatemanager as acm,
+)
 from aws_cdk.aws_apigateway import (
     ApiKey,
     UsagePlan,
-    ThrottleSettings)
+    ThrottleSettings, SecurityPolicy, EndpointType)
 from aws_cdk.aws_ecr import Repository
 from constructs import Construct
 
@@ -25,6 +27,12 @@ class HelloLambdaStack(Stack):
                                                        "hello-lambda"),
             tag_or_digest=image_tag
         )
+        hello_lambda_domain_name = "helloworld-lambda.bullyrooks.com"
+        # Create a certificate for the domain name
+        hello_lambda_certificate = acm.Certificate(self, "hellworld-lambda_certificate",
+                                                   domain_name=hello_lambda_domain_name,
+                                                   validation=acm.CertificateValidation.from_dns(self))
+
         hello_lambda_lambda = _lambda.DockerImageFunction(
             scope=self,
             id="hello-lambda-lambda",
@@ -41,6 +49,15 @@ class HelloLambdaStack(Stack):
                                                     proxy=False,
                                                     api_key_source_type=apigateway.ApiKeySourceType.HEADER
                                                     )
+
+        hello_lambda_api.add_domain_name(
+            id="helloworld-lambda-domain",
+            domain_name=hello_lambda_domain_name,
+            certificate=hello_lambda_certificate,
+            security_policy=SecurityPolicy.TLS_1_2,
+            endpoint_type=EndpointType.REGIONAL,
+            mapping=None
+        )
 
         hello_lambda_api.root.add_method("GET", api_key_required=True)
 
@@ -66,4 +83,4 @@ class HelloLambdaStack(Stack):
         development_usage_plan.add_api_stage(
             api=hello_lambda_api,
             stage=hello_lambda_api.deployment_stage
-)
+        )
